2f0d37d0b14dfba6765e2cd1f59636527ff8d8cb
ponybloodseeker <32543778+ponybloodseeker@users.noreply.github.com>
2019-11-04 11:10:45 +0300

    The stable version of the support bot

---
+++ index.js
@@ -0,0 +1,158 @@
+'use strict';
+
+// Imports dependencies and set up http server
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json()); // creates express http server
+const request = require('request');
+
+const PAGE_ACCESS_TOKEN = "EAAHhK7oMsDcBAOi23rCS3rOx3EJX1Rz3X2GJyo4YDvj1M3ZCYBRvl0p65Rg416uZCvWPp7QRXVOuSZBYoQ9ffZCOGUFxwIVxBSZAr0PRL3hgNsQV9A19mcMYXG2XmTY9iTKUkCZBwXQoBSlG3F38WfdLojwBQZBteW0eI0YtdurYv7SrsKVTv3W";
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 80, () => console.log('webhook is listening'));
+
+function callMessagingAPI(sender_psid, response) {
+    let structured = false;
+    if(response.text.localeCompare("ask me") == 0) {
+        structured = true;
+    }
+    let withSpaces = "";
+    for(var i = 0; i < response.text.length; i++) {
+        withSpaces += response.text[i] + " ";
+    }
+    response.text = withSpaces;
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": {
+            "text": response.text 
+        }
+    }
+    if(structured) {
+        response = {
+            "attachment": {
+              "type": "template",
+              "payload": {
+                "template_type": "generic",
+                "elements": [{
+                  "title": "Is Star Wars your favoriet movie?",
+                  "subtitle": "Try to give the correct answer",
+                  "image_url": "https://theplaylist.net/wp-content/uploads/2019/05/Natalie-Portman-Star-Wars-Phantom-Menace-1200x520.jpg",
+                  "buttons": [
+                    {
+                        "type": "postback",
+                        "title": "Yes, it is",
+                        "payload": "yes",
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Of course!",
+                        "payload": "yes",
+                    },
+                    {
+                        "type": "postback",
+                        "title": "It sucks",
+                        "payload": "no",
+                    }
+                  ],
+                }]
+              }
+            }
+          }
+        request_body.message = response;
+    }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+        console.log('message sent!'); 
+    } else {
+        console.error("Unable to send message:" + err);
+    }
+  }); 
+}
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+    console.log("SENDER ID: " + sender_psid);
+    console.log(received_message);
+    callMessagingAPI(sender_psid, received_message);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+    let message = "Krasavchik";
+    if(received_postback.payload == "no") {
+        message = "Тогда иди дери свою мамашу";
+    }
+    callMessagingAPI(sender_psid, {"text": message});
+}
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "fide_support_bot_verify_token"
+      
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+      
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+    
+      // Checks the mode and token sent is correct
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
+    }
+  });
+
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+ 
+    let body = req.body;
+  
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+  
+      // Iterates over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+  
+        // Gets the message. entry.messaging is an array, but 
+        // will only ever contain one message, so we get index 0
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+
+        let sender_psid = webhook_event.sender.id;
+        if(webhook_event.message) {
+            handleMessage(sender_psid, webhook_event.message);
+        }else if(webhook_event.postback) {
+            handlePostback(sender_psid, webhook_event.postback);
+        }
+      });
+  
+      // Returns a '200 OK' response to all requests
+      res.status(200).send('EVENT_RECEIVED');
+    } else {
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+  
+  });
